2e12747e096bcbd56fd78cc8dd79fea00131bdd4
Mafrans <malte.kluft@gmail.com>
2020-11-18 22:38:16 +0100

    Simple Vue Clicker

---
+++ public/javascripts/worker.js
@@ -0,0 +1,16 @@
+Vue.component('worker', {
+    template: `
+        <div class="worker card uk-margin-small-bottom uk-flex uk-flex-middle">
+            <div class="uk-flex uk-flex-between uk-flex-column uk-height-1-1">
+                <h3 class="uk-margin-remove">{{ name }}</h3>
+                <p class="uk-margin-remove">
+                    $<span v-text="speed"></span> / sec
+                </p>
+            </div>
+            <h3 class="uk-margin-remove-top uk-margin-remove-bottom uk-margin-auto-left">
+                $<span v-text="cost"></span>
+            </h3>
+        </div>
+    `,
+    props: ['name', 'speed', 'cost']
+})
